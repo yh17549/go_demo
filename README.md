c8ffcb7e646e536960f1c5c98157edfb3dcba370
yh17549 <1754921231@qq.com>
2019-07-10 22:41:50 +0800

    add ShareFile
    Signed-off-by: yh17549 <1754921231@qq.com>

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# go_demo
+# go_tool
+
+## shareFile.go
+
+* description
+
+  > the tool for share files on same network
+
+* how to use
+
+  ```sh
+  $ go build shareFile.go
+  $ ./shareFile
+  ```
+
+* help
+
+  ```sh
+  $ ./shareFile -h
+  ```
---
+++ ShareFile.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"html/template"
+	"io/ioutil"
+	"log"
+	"net"
+	"net/http"
+	"os"
+	"time"
+)
+
+var workDir string
+
+func handerListFile(w http.ResponseWriter, r *http.Request) {
+
+	dir_list, e := ioutil.ReadDir(workDir)
+	if e != nil {
+		fmt.Println("read dir error")
+		return
+	}
+
+	var fileListTemplate = template.
+		Must(template.New("filelist").
+			Parse(`
+<h1>file list</h1>
+<table>
+	<tr style='text-align: left'>
+		<th>Title</th>
+	</tr>
+	{{range .}}
+	<tr>
+		<td> <a href='file/{{.Name}}'>{{.Name}}</a> </td>
+	</tr>
+	{{end}}
+</table>
+`))
+	fileListTemplate.Execute(w, dir_list)
+}
+
+var port string
+var autoShutdown bool
+var shutdownMinute int
+
+func init() {
+	flag.StringVar(&port, "port", "8809", "port")
+	flag.BoolVar(&autoShutdown, "autoShutdown", true, "application will shutdown after a specified time")
+	flag.IntVar(&shutdownMinute, "shutdownMinute", 5, "the specified time (minute)")
+}
+
+func main() {
+
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage :\n")
+		flag.PrintDefaults()
+		fmt.Println("Example:")
+		fmt.Println("  sh ./shareFile -port 8890 -shutdownMinute 1")
+	}
+	flag.Parse()
+
+	var err error
+	workDir, err = os.Getwd()
+	if err != nil {
+		fmt.Println("get work dir error", err)
+		return
+	}
+
+	fmt.Println("share dir:", workDir)
+
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	http.HandleFunc("/", handerListFile)
+	http.Handle("/file/", http.StripPrefix("/file/", http.FileServer(http.Dir(workDir))))
+	for _, value := range addrs {
+		if ipnet, ok := value.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				listenUrl := ipnet.IP.String() + ":" + port
+				fmt.Println("please visit: http://" + listenUrl)
+				go func(listenUrl string) {
+
+					log.Fatal(http.ListenAndServe(listenUrl, nil))
+				}(listenUrl)
+			}
+		}
+	}
+
+	fmt.Println("file is sharing...")
+
+	if autoShutdown {
+		now := time.Now()
+
+		next := now.Add(time.Minute * time.Duration(shutdownMinute))
+
+		t := time.NewTimer(next.Sub(now))
+		<-t.C
+		fmt.Println("auto close application after ", shutdownMinute, " minutes")
+	} else {
+		select {}
+	}
+
+}
